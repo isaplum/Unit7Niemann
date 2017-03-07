aa3a72a0b631abd735bccc67db50744dc4dd4b0e
krixniemann <krixniemann@css.edu>
2017-03-06 19:29:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/krixniemann/unit7niemann/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.krixniemann.unit7niemann;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText rmwenter;
+    EditText rmhenter;
+    TextView resulty;
+    private static final int CIS3334_REQUEST_CODE = 1001;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       resulty = (TextView) findViewById(R.id.txtResult);
+        rmwenter = (EditText) findViewById(R.id.rmwedit);
+        rmhenter = (EditText) findViewById(R.id.rmhedit);
+    }
+
+    public void onBtnClick(View view){
+        int width = Integer.parseInt(String.valueOf(rmwenter.getText()));
+        int height = Integer.parseInt(String.valueOf(rmhenter.getText()));
+        Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+        intent.putExtra("Width", width);
+        intent.putExtra("Height", height);
+        startActivityForResult(intent, CIS3334_REQUEST_CODE);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == CIS3334_REQUEST_CODE) {
+            if (data.hasExtra("Flooring")) {
+                String result = data.getExtras().getString("Flooring");
+                if (result != null && result.length() > 0) {
+                    resulty.setText("Flooring : " + result);
+                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/krixniemann/unit7niemann/OtherActivity.java
@@ -0,0 +1,40 @@
+package com.example.krixniemann.unit7niemann;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by krixniemann on 3/6/2017.
+ */
+
+public class OtherActivity extends AppCompatActivity {
+
+    TextView textViewData;
+    int width;
+    int height;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout2);
+
+        textViewData = (TextView) findViewById(R.id.txtDisplay);
+
+        Bundle extras = getIntent().getExtras();
+        width = extras.getInt("Width");
+        height = extras.getInt("Height");
+        textViewData.setText("Width is " + width + " the length is " + height + " and flooring needed is " +(width * height));
+
+    }
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+        intent.putExtra("Flooring", (width*height));
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
+}
